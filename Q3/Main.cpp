0b2fa8424f74bd78742135722f77e8f3b8723bd5
Serj Qazi <sirajum.qazi@gmail.com>
2025-08-02 02:37:36 -0400

    initial commit

---
+++ Q3/Main.cpp
@@ -0,0 +1,9 @@
+/* Define a function hypotenuse that calculates the hypotenuse of a right triangle
+when the other two sides are given. The function should take two double arguments
+and return the hypotenuse as a double. Use this function in a program to determine
+the hypotenuse for each of the triangles shown below. 
+
+Triangle	Side 1		Side 2
+1			3.0			4.0
+2			5.0			12.0
+3			8.0			15.0 */
---
+++ Q4/Main.cpp
@@ -0,0 +1,4 @@
+/* Write a program that inputs a series of integers and passes them one at a time to
+function isEven, which uses the modulus operator to determine whether an integer
+is even. The function should take an integer argument and return true if the integer
+is even and false otherwise. */
